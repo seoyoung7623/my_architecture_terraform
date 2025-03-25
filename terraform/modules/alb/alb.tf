2c0212ab2b33351f08a45f9042b03fb6f54d93c2
SeoYoung <seoyoung7623@naver.com>
2025-03-25 15:24:39 +0900

    feat: ALB 모듈

---
+++ terraform/modules/alb/alb.tf
@@ -0,0 +1,161 @@
+## SSL/TLS 인증서 ARN
+data "aws_acm_certificate" "certificate" {
+    domain = "seoyoungstudy.shop"
+    statuses = ["ISSUED"]
+    most_recent = true
+}
+
+
+# ALB 생성
+resource "aws_alb" "alb" {
+    name = "aws-alb-${var.stage}-${var.servicename}"
+    internal = var.internal
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.sg-alb.id]
+    subnets = var.subnet_ids
+
+    enable_deletion_protection = false # 삭제 보호 기능 활성화
+
+    idle_timeout = var.idle_timeout
+
+    # # ALB 접근 로그 S3에 저장
+    # access_logs {
+    #     bucket = var.aws_s3_lb_logs_name
+    #     prefix = "aws-alb-${var.stage}-${var.servicename}" # 파일의 접두사
+    #     enabled = true # 접근 로그 기능 활성화
+    # }
+
+    tags = merge(tomap({
+        Name = "aws-alb-${var.stage}-${var.servicename}"}),
+        var.tags
+    )
+}
+
+# ALB Listener 생성
+resource "aws_alb_listener" "alb_listener-80" {
+    load_balancer_arn = aws_alb.alb.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+        type = "forward"
+        target_group_arn = aws_alb_target_group.alb_target_group.arn
+    }
+}
+
+resource "aws_alb_listener" "alb-listener-443" {
+    load_balancer_arn = aws_alb.alb.arn
+    port = 443
+    protocol = "HTTPS"
+
+    ssl_policy = "ELBSecurityPolicy-2016-08" # SSL/TLS 프로토콜 버전
+    certificate_arn = data.aws_acm_certificate.certificate.arn # SSL/TLS 인증서 ARN
+
+    default_action {
+        type = "forward"
+        target_group_arn = aws_alb_target_group.alb_target_group.arn
+    }
+}
+
+# ALB Target Group 생성
+resource "aws_alb_target_group" "alb_target_group" {
+    name = "aws-alb-tg-${var.stage}-${var.servicename}"
+    port = var.port
+    protocol = "HTTP"
+    vpc_id = var.vpc_id
+    target_type = var.target_type
+
+    health_check {
+        path = var.hc_path
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 5
+        healthy_threshold = var.hc_healty_threshold
+        unhealthy_threshold = var.hc_unhealty_threshold
+    }
+
+    tags = merge(tomap({
+        Name = "aws-alb-tg-${var.stage}-${var.servicename}"}),
+        var.tags
+    )
+}
+
+# ALB 보안그룹 생성
+resource "aws_security_group" "sg-alb" {
+    name = "aws-alb-${var.stage}-${var.servicename}"
+    vpc_id = var.vpc_id
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "TCP"
+        cidr_blocks = var.sg_allow_comm_list
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = var.sg_allow_comm_list
+        # self = true # 내부 통신을 원하면 주석 해제
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = merge(tomap({
+        Name = "sg-alb-${var.stage}-${var.servicename}"}),
+        var.tags
+    )
+}
+
+resource "aws_security_group" "sg-alb-to-tg" {
+  name = "aws-alb-to-tg-${var.stage}-${var.servicename}"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = var.port
+    to_port = var.port
+    protocol = "TCP"
+    security_groups = [aws_security_group.sg-alb.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(tomap({
+    Name = "sg-alb-to-tg-${var.stage}-${var.servicename}"}),
+    var.tags
+  )
+}
+
+# ASG 생성
+resource "aws_autoscaling_group" "aws_asg" {
+  name = "aws-asg-${var.stage}-${var.servicename}"
+    vpc_zone_identifier = var.asg-subnets # 프라이빗 서브넷에 넣었음
+    min_size = 1
+    max_size = 3
+    launch_template {
+        id = var.launch_template_id
+        version = "$Latest"
+    }
+
+    instance_refresh {
+        strategy = "Rolling"
+        preferences {
+        min_healthy_percentage = 50
+        instance_warmup        = 60
+        }
+    }
+    # health_check_type = "ELB"
+    target_group_arns = [aws_alb_target_group.alb_target_group.arn]
+}
---
+++ terraform/modules/alb/variables.tf
@@ -0,0 +1,72 @@
+variable "stage" {
+    type  = string
+    default = "dev"
+}
+variable "servicename" {
+    type  = string
+    default = "seoyoung"
+}
+variable "tags" {
+  type = map(string)
+  default = {
+    "name" = "seoyoung-alb"
+  }
+}
+
+variable "internal" {
+    type  = bool
+    # default = true # 내부망을 사용하려면 주석 해제
+    default = false 
+}
+
+variable "subnet_ids" {
+    type  = list
+    default = []
+}
+
+variable "idle_timeout" {
+    type  = number
+    default = 60
+}
+
+variable "vpc_id" {
+    type  = string
+}
+
+variable "aws_s3_lb_logs_name" {
+    type  = string
+}
+
+# Health Check
+variable "port" {
+    type  = number
+}
+
+variable "target_type" {
+    type  = string
+}
+
+variable "hc_path" {
+  type = string
+}
+
+variable "hc_healty_threshold" {
+    type = number
+}
+
+variable "hc_unhealty_threshold" {
+  type = number
+}
+
+variable "sg_allow_comm_list" {
+  type = list(string)
+}
+
+variable "asg-subnets" {
+  type = list(string)
+}
+
+# Launch Template
+variable "launch_template_id" {
+    type = string
+}
