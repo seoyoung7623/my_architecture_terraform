53c64389c358f8d8bc3d9cea359fd8470e791628
SeoYoung <seoyoung7623@naver.com>
2025-03-26 10:18:11 +0900

    feat: wordpress 적용

--- modules/launch_template/launch_template.tf
+++ modules/launch_template/launch_template.tf
@@ -5,10 +5,19 @@ resource "aws_launch_template" "aws_lt" {
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
-    sudo apt update -y
-    sudo apt install -y nginx
-    sudo systemctl start nginx
-    sudo systemctl enable nginx
+    apt update -y
+    apt remove -y nginx nginx-common
+    apt install -y apache2 php php-mysql mysql-client unzip wget
+
+    cd /var/www/html
+    wget https://wordpress.org/latest.zip
+    unzip latest.zip
+    cp -r wordpress/* .
+    chown -R www-data:www-data /var/www/html
+    chmod -R 755 /var/www/html
+
+    systemctl restart apache2
+    systemctl enable apache2
     EOF
   )
 
