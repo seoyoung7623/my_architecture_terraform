566b04153a4e69f8dc9cf33f8ad6baf763ce362c
SeoYoung <seoyoung7623@naver.com>
2025-03-30 17:36:26 +0900

    fix: route53 도메인 open

--- modules/route53/route53.tf
+++ modules/route53/route53.tf
@@ -3,17 +3,17 @@ data "aws_route53_zone" "seoyoungstudy" {
   private_zone = false
 }
 
-# resource "aws_route53_record" "alb_alias" {
-#   zone_id = data.aws_route53_zone.seoyoungstudy.zone_id         # 도메인 호스팅 영역 ID
-#   name    = var.host_name              # 연결할 도메인 이름
-#   type    = var.record_type             # 레코드 유형
+resource "aws_route53_record" "alb_alias" {
+  zone_id = data.aws_route53_zone.seoyoungstudy.zone_id         # 도메인 호스팅 영역 ID
+  name    = var.host_name              # 연결할 도메인 이름
+  type    = var.record_type             # 레코드 유형
 
-#   alias {
-#     name                   = var.alb_dns_name   # ALB DNS 주소
-#     zone_id                = var.alb_zone_id    # ALB의 호스팅 영역 ID
-#     evaluate_target_health = true
-#   }
-# }
+  alias {
+    name                   = var.alb_dns_name   # ALB DNS 주소
+    zone_id                = var.alb_zone_id    # ALB의 호스팅 영역 ID
+    evaluate_target_health = true               # ALB의 상태를 평가하여 상태가 정상이면 트래픽을 전달
+  }
+}
 
 resource "aws_route53_record" "frontend_alias" {
   zone_id = data.aws_route53_zone.seoyoungstudy.zone_id
@@ -23,7 +23,7 @@ resource "aws_route53_record" "frontend_alias" {
   alias {
     name                   = var.front_dns_name
     zone_id                = var.front_hosted_zone_id
-    evaluate_target_health = false
+    evaluate_target_health = false 
   }
 }
 
