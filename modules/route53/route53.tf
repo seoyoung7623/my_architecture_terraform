265cb9252efe2273f0f84d08339b39c622f15f22
SeoYoung <seoyoung7623@naver.com>
2025-03-30 16:44:22 +0900

    fix: 도메인 ALB -> Cloudfront

--- modules/route53/route53.tf
+++ modules/route53/route53.tf
@@ -3,14 +3,27 @@ data "aws_route53_zone" "seoyoungstudy" {
   private_zone = false
 }
 
-resource "aws_route53_record" "alb_alias" {
-  zone_id = data.aws_route53_zone.seoyoungstudy.zone_id         # 도메인 호스팅 영역 ID
-  name    = var.host_name              # 연결할 도메인 이름
-  type    = var.record_type             # 레코드 유형
+# resource "aws_route53_record" "alb_alias" {
+#   zone_id = data.aws_route53_zone.seoyoungstudy.zone_id         # 도메인 호스팅 영역 ID
+#   name    = var.host_name              # 연결할 도메인 이름
+#   type    = var.record_type             # 레코드 유형
+
+#   alias {
+#     name                   = var.alb_dns_name   # ALB DNS 주소
+#     zone_id                = var.alb_zone_id    # ALB의 호스팅 영역 ID
+#     evaluate_target_health = true
+#   }
+# }
+
+resource "aws_route53_record" "frontend_alias" {
+  zone_id = data.aws_route53_zone.seoyoungstudy.zone_id
+  name    = var.dev_domain_name
+  type    = var.record_type
 
   alias {
-    name                   = var.alb_dns_name   # ALB DNS 주소
-    zone_id                = var.alb_zone_id    # ALB의 호스팅 영역 ID
-    evaluate_target_health = true
+    name                   = var.front_dns_name
+    zone_id                = var.front_hosted_zone_id
+    evaluate_target_health = false
   }
 }
+
